2d366650365aa8f946ebe9bf5965133548e02ad8
Chirag Khatawkar <71436190+Chirag-khatawkar@users.noreply.github.com>
2020-11-05 15:20:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -34,7 +34,7 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(600, 200);
+  createCanvas(windowWidth,200);
 
   trex = createSprite(50,180,20,50);
 
@@ -59,7 +59,7 @@ function setup() {
   gameOver.visible = false;
   restart.visible = false;
 
-  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround = createSprite(width/2,190,width,10);
   invisibleGround.visible = false;
 
   cloudsGroup = new Group();
@@ -124,7 +124,7 @@ function draw() {
 function spawnClouds() {
   //write code here to spawn the clouds
   if (frameCount % 60 === 0) {
-    var cloud = createSprite(600,120,40,10);
+    var cloud = createSprite(width,120,40,10);
     cloud.y = Math.round(random(80,120));
     cloud.addImage(cloudImage);
     cloud.scale = 0.5;
@@ -145,7 +145,7 @@ function spawnClouds() {
 
 function spawnObstacles() {
   if(frameCount % 60 === 0) {
-    var obstacle = createSprite(600,165,10,40);
+    var obstacle = createSprite(width,165,10,40);
     //obstacle.debug = true;
     obstacle.velocityX = -(6 + 3*score/100);
     
